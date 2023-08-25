857872ebebfc3a89313426bbe14f47674d4e38db
LuvSuresh <preetasuresh9@gmail.com>
2023-08-25 16:07:55 -0700

    Initial commit

---
+++ app/src/main/java/com/firstapp/navigationbar/MainActivity.java
@@ -0,0 +1,18 @@
+package com.firstapp.navigationbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
